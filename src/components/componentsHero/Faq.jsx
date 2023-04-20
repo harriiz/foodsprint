9294ce456fd3b27daedcd2d9bc1720b6215ae23f
harriiz <hariz.kasumovic@gmail.com>
2023-04-20 23:42:04 +0200

    Promijenjena FAQ sekcija

--- src/components/componentsHero/Faq.jsx
+++ src/components/componentsHero/Faq.jsx
@@ -14,7 +14,7 @@ function Faq() {
   const useStyles = createStyles((theme) => ({
     wrapper: {
       paddingTop: `calc(${theme.spacing.xl} * 2)`,
-      minHeight: rem(820),
+      minHeight: rem(720),
       backgroundImage: `radial-gradient(${
         theme.colors[theme.primaryColor][6]
       } 0%, ${theme.colors[theme.primaryColor][4]} 100%)`,
@@ -92,7 +92,7 @@ function Faq() {
       <div className={classes.wrapper}>
         <Container size="sm">
           <Title align="center" className={classes.title}>
-            Frequently Asked Questions
+            Često Postavljena Pitanja
           </Title>
 
           <Accordion
@@ -107,39 +107,83 @@ function Faq() {
               </ThemeIcon>
             }
           >
-            <Accordion.Item className={classes.item} value="reset-password">
-              <Accordion.Control>
-                How can I reset my password?
-              </Accordion.Control>
-              <Accordion.Panel>{placeholder}</Accordion.Panel>
+            <Accordion.Item className={classes.item} value="kreiranje-naloga">
+              <Accordion.Control>Kako mogu da kreiram nalog?</Accordion.Control>
+              <Accordion.Panel>
+                Za kreiranje naloga potrebno je da kliknete na dugme "Registruj
+                se" na početnoj stranici aplikacije i popunite obrazac sa
+                traženim podacima. Nakon toga će biti potrebno da verifikujete
+                svoj nalog putem email-a ili SMS-a.
+              </Accordion.Panel>
             </Accordion.Item>
 
-            <Accordion.Item className={classes.item} value="another-account">
+            <Accordion.Item className={classes.item} value="napomene">
               <Accordion.Control>
-                Can I create more that one account?
+                Da li je moguće ostaviti napomenu uz porudžbinu?
               </Accordion.Control>
-              <Accordion.Panel>{placeholder}</Accordion.Panel>
+              <Accordion.Panel>
+                Da, naša aplikacija omogućava korisnicima da ostave napomenu uz
+                porudžbinu, kao što su alergije na hranu, posebni zahtevi za
+                pripremu hrane ili željene temperature pića.
+              </Accordion.Panel>
             </Accordion.Item>
 
-            <Accordion.Item className={classes.item} value="newsletter">
+            <Accordion.Item className={classes.item} value="pracenje-dostave">
               <Accordion.Control>
-                How can I subscribe to monthly newsletter?
+                Da li je moguće pratiti dostavu u realnom vremenu?
               </Accordion.Control>
-              <Accordion.Panel>{placeholder}</Accordion.Panel>
+              <Accordion.Panel>
+                Da, naša aplikacija omogućava korisnicima da prate dostavu u
+                realnom vremenu. Nakon što je porudžbina prihvaćena, korisnicima
+                će biti omogućeno praćenje dostave sa informacijama o trenutnoj
+                lokaciji dostavljača na njihovom profilu.
+              </Accordion.Panel>
             </Accordion.Item>
 
-            <Accordion.Item className={classes.item} value="credit-card">
+            <Accordion.Item className={classes.item} value="povratak">
               <Accordion.Control>
-                Do you store credit card information securely?
+                Da li mogu da vratim poručenu hranu?
               </Accordion.Control>
-              <Accordion.Panel>{placeholder}</Accordion.Panel>
+              <Accordion.Panel>
+                Ukoliko postoji neki problem sa poručenom hranom, potrebno je da
+                se obratite našem timu za podršku. U nekim slučajevima je moguće
+                vratiti hranu uz povraćaj novca, ali ovo zavisi od specifičnih
+                pravila prodavca i politike naše aplikacije
+              </Accordion.Panel>
             </Accordion.Item>
 
-            <Accordion.Item className={classes.item} value="payment">
+            <Accordion.Item className={classes.item} value="vrste-restorana">
               <Accordion.Control>
-                What payment systems to you work with?
+                Koje vrste restorana su dostupne na vašoj platformi?
               </Accordion.Control>
-              <Accordion.Panel>{placeholder}</Accordion.Panel>
+              <Accordion.Panel>
+                Na našoj platformi možete pronaći različite vrste restorana,
+                uključujući restorane brze hrane, restorane sa lokalnom
+                kuhinjom, veganske i vegetarijanske restorane, restorane sa
+                internacionalnom kuhinjom i druge.
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="adresa-isporuke">
+              <Accordion.Control>
+                Da li mogu da naručim hranu za isporuku na drugu adresu?
+              </Accordion.Control>
+              <Accordion.Panel>
+                Da, naša aplikacija omogućava korisnicima da naruče hranu za
+                dostavu na drugu adresu. Potrebno je da unesete adresu dostave
+                prilikom poručivanja.
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="historija-narudzbi">
+              <Accordion.Control>
+                Kako da pretražim svoju historiju porudžbina?
+              </Accordion.Control>
+              <Accordion.Panel>
+                Da biste pregledali svoju historiju porudžbina, kliknite na
+                ikonu "istorija porudžbina" u vašem profilu. Tu ćete moći da
+                vidite sve vaše porudžbine, kao i detalje o njima.
+              </Accordion.Panel>
             </Accordion.Item>
           </Accordion>
         </Container>
